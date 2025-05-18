c92d30677d7e9b5ad1738d080e6a62398ff1d4f6
skyaironline <aldo@batamon.asia>
2025-05-18 16:18:18 +0700

    script and unit test for minimal account

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -1,10 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-import {Script} from "forge-std/Script.sol";
+import {Script, console} from "forge-std/Script.sol";
+import {EntryPoint} from "lib/account-abstraction/contracts/core/EntryPoint.sol";
 
 contract HelperConfig is Script {
-    error HelperCongig__InvalidChainId();
+    error HelperConfig__InvalidChainId();
 
     struct NetworkConfig {
         address entryPoint;
@@ -14,8 +15,10 @@ contract HelperConfig is Script {
     uint256 constant ETH_SEPOLIA_CHAIN_ID = 11155111;
     uint256 constant ZKSYNC_SEPOLIA_CHAIN_ID = 300;
     uint256 constant LOCAL_CHAIN_ID = 31337;
-    address constant BURNER_WALLET = 0x1b940229051d6ccC410f4A03962D846ED60bcc59;
-    address constant FOUNDRY_DEFAULT_WALLET = 0x1804c8AB1F12E6bbf3894d4083f33e07309d1f38;
+    address constant BURNER_WALLET = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
+    // address constant FOUNDRY_DEFAULT_WALLET = 0x1804c8AB1F12E6bbf3894d4083f33e07309d1f38;
+    address constant ANVIL_DEFAULT_ACCOUNT =
+        0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
 
     NetworkConfig public localNetworkConfig;
     mapping(uint256 chainId => NetworkConfig) public networkConfigs;
@@ -28,28 +31,32 @@ contract HelperConfig is Script {
         return getConfigByChainId(block.chainid);
     }
 
-    function getConfigByChainId(uint256 chainId) public returns (NetworkConfig memory) {
+    function getConfigByChainId(
+        uint256 chainId
+    ) public returns (NetworkConfig memory) {
         if (chainId == LOCAL_CHAIN_ID) {
             return getOrCreateAnvilEthConfig();
         } else if (networkConfigs[chainId].account != address(0)) {
             return networkConfigs[chainId];
         } else {
-            revert HelperCongig__InvalidChainId();
+            revert HelperConfig__InvalidChainId();
         }
     }
 
     function getEthSepoliaConfig() public pure returns (NetworkConfig memory) {
-        return NetworkConfig({
-            entryPoint: 0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789,
-            account: BURNER_WALLET
-        });
+        return
+            NetworkConfig({
+                entryPoint: 0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789,
+                account: BURNER_WALLET
+            });
     }
 
-    function getZkSyncSepoliaConfig() public pure returns (NetworkConfig memory) {
-        return NetworkConfig({
-            entryPoint: address(0),
-            account: BURNER_WALLET
-        });
+    function getZkSyncSepoliaConfig()
+        public
+        pure
+        returns (NetworkConfig memory)
+    {
+        return NetworkConfig({entryPoint: address(0), account: BURNER_WALLET});
     }
 
     function getOrCreateAnvilEthConfig() public returns (NetworkConfig memory) {
@@ -58,7 +65,14 @@ contract HelperConfig is Script {
         }
 
         // deploy mocks
-        
-        return NetworkConfig({entryPoint: address(0), account: FOUNDRY_DEFAULT_WALLET});
+        console.log("Deploying mocks...");
+        vm.startBroadcast(ANVIL_DEFAULT_ACCOUNT);
+        EntryPoint entryPoint = new EntryPoint();
+        vm.stopBroadcast();
+        console.log("Mocks deployed!");
+
+        localNetworkConfig =
+            NetworkConfig({entryPoint: address(entryPoint), account: ANVIL_DEFAULT_ACCOUNT});
+        return localNetworkConfig;
     }
 }
